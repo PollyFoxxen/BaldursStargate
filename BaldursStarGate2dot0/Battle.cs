61daa7ed169e6422611347680070e6afc4421c3f
HenrikVP <hpo@tec.dk>
2023-09-06 12:57:16 +0200

    battle end (so far) VI

---
+++ BaldursStarGate2dot0/Battle.cs
@@ -0,0 +1,70 @@
+﻿namespace BaldursStarGate2dot0
+{
+    internal class Battle
+    {
+        public Battle(Player player, Monster monster)
+        {
+            Combat(player, monster);
+        }
+
+        private void Combat(Player player, Monster monster)
+        {
+            Console.WriteLine("Player opens a chest, and out jumps a... " + monster.Type);
+            bool continueBattle = true;
+            int counter = 1;
+            while (continueBattle)
+            {
+                Console.WriteLine("Round " + counter++);
+                Console.ReadKey();
+                if (WhoStarts() == 0)
+                {
+                    continueBattle = Attack(player, monster);
+                    if (!continueBattle) break;
+                    continueBattle = Attack(monster, player);
+                }
+                else
+                {
+                    continueBattle = Attack(monster, player);
+                    if (!continueBattle) break;
+                    continueBattle = Attack(player, monster);
+                }
+            }
+        }
+
+        private bool Attack(Creature attacker, Creature defender)
+        {
+            int att = AttackRoll();
+            Console.WriteLine(attacker.Type + " swings, and rolls a " + att);
+            att = ArmorReduction(att, defender);
+            Console.WriteLine(defender.Type + "'s armor reduces attack to " + att);
+            defender.ReduceHealth(att);
+            Console.WriteLine($"{defender.Type} have {defender.Health} health left.");
+
+            //We check if someone died, and if, we return false, so we can stop the battle
+            if (defender.Health <= 0)
+            {
+                Console.WriteLine(defender.Type + " have died");
+                return false;
+            }
+
+            return true;
+        }
+
+        private int ArmorReduction(int att, Creature creature)
+        {
+            return Math.Max(0, att - creature.Armor);
+        }
+
+        //TODO Add modifier dependent on weapon or defender 
+        private int AttackRoll()
+        {
+            int att = Game.Rnd.Next(Game.Dice);
+            return att;
+        }
+
+        private int WhoStarts()
+        {
+            return Game.Rnd.Next(2);
+        }
+    }
+}
--- BaldursStarGate2dot0/Game.cs
+++ BaldursStarGate2dot0/Game.cs
@@ -2,77 +2,18 @@
 {
     internal class Game
     {
-        int dice = 20;
+        public static int Dice = 20;
         //TODO Cleanup (refactoring)
         //TODO Choose weapons
-        private Random rnd = new Random();
+        //Save in files
+        public static Random Rnd = new Random();
 
         public Game()
         {
             Console.WriteLine("We have started our game constructor by instantiating an object");
             Player pg = CreatePlayer();
             Monster mg = CreateMonster();
-            Battle(pg, mg);
-        }
-
-        private void Battle(Player player, Monster monster)
-        {
-            Console.WriteLine("Player opens a chest, and out jumps a... " + monster.Type);
-            bool continueBattle = true;
-            int counter = 1;
-            while (continueBattle)
-            {
-                Console.WriteLine("Round " + counter++);
-                Console.ReadKey();
-                if (WhoStarts() == 0)
-                {
-                    continueBattle = Attack(player, monster);
-                    if (!continueBattle) break;
-                    continueBattle = Attack(monster, player);
-                }
-                else
-                {
-                    continueBattle = Attack(monster, player);
-                    if (!continueBattle) break;
-                    continueBattle = Attack(player, monster);
-                }
-            }
-        }
-
-        private bool Attack(Creature attacker, Creature defender)
-        {
-            int att = AttackRoll();
-            Console.WriteLine(attacker.Type + " swings, and rolls a " + att);
-            att = ArmorReduction(att, defender);
-            Console.WriteLine(defender.Type + "'s armor reduces attack to " + att);
-            defender.ReduceHealth(att);
-            Console.WriteLine($"{defender.Type} have {defender.Health} health left.");
-
-            //We check if someone died, and if, we return false, so we can stop the battle
-            if (defender.Health <= 0) 
-            { 
-                Console.WriteLine(defender.Type + " have died");
-                return false;
-            }
-            
-            return true;
-        }
-
-        private int ArmorReduction(int att, Creature creature)
-        {
-            return Math.Max(0, att - creature.Armor);
-        }
-
-        //TODO Add modifier dependent on weapon or defender 
-        private int AttackRoll()
-        {
-            int att = rnd.Next(dice);
-            return att;
-        }
-
-        private int WhoStarts()
-        {
-            return rnd.Next(2);
+            new Battle(pg, mg);
         }
 
         private Player CreatePlayer()
@@ -89,6 +30,5 @@
         {
             return new Monster();
         }
-
     }
 }
