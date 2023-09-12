4ccaab1e8d8ca2769bfff92f8139ca5cde026e47
PollyFoxxen <nann4905@outlook.com>
2023-09-12 13:14:43 +0200

    wooohoooo

---
+++ BaldursStarGate2dot0/Equipment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaldursStarGate2dot0
+{
+    internal class Equipment
+    {
+        public string? Name { get; set; }
+        public int Armor { get; set; }
+        public int Damage { get; set; }
+        public int Price { get; set; }
+    }
+}
